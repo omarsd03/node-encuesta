d52e8ad3d1b3a176a72256b393fcc5c9312a3e0a
OmarSD <salgadodiaz_wwe@outlook.com>
2020-06-07 21:39:49 -0500

    first commit

---
+++ server/models/employee.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const employeeSchema = new Schema({
+    sgi: { type: String, required: true },
+    primerPregunta: { type: String, required: true },
+    segundaPregunta: { type: String, required: true },
+    tercerPregunta: { type: String, required: true },
+    cuartaPregunta: { type: String, required: true },
+    quintaPregunta: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
